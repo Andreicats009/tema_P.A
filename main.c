64001f8a31beaa5c3f15f7af243eed9d699efdb1
Dennis Gherghina <65402629+Andreicats009@users.noreply.github.com>
2026-03-06 18:48:02 +0200

    structura persoane

--- main.c
+++ main.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define lungime_max 50
+#define hobby_max 10
+
+typedef struct {
+    char nume[lungime_max];
+    char prenume[lungime_max];
+    char oras[lungime_max];
+    char liceu[lungime_max];
+} persoana;
+
 
 int main(void)
 {
